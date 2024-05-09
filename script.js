0bdfc6a18ebe88d28b6ae0ecd957132cbd32eb01
istvan-tamas <istvan.tamas90@gmail.com>
2024-05-09 19:15:54 +0200

    Final touches

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let button = document.querySelector('#joke-btn');
+
 let loadJoke = () => {
 	let joke = new XMLHttpRequest();
-	joke.responseType = 'text';
 	joke.onreadystatechange = function () {
 		if (joke.readyState === 4 && joke.status === 200) {
 			let data = JSON.parse(joke.responseText);
+			attachImg('https://imgflip.com/s/meme/Chuck-Norris-Guns.jpg');
 			document.querySelector('#joke').innerHTML = data.value;
 		}
 	};
@@ -11,5 +13,13 @@ let loadJoke = () => {
 	joke.send(null);
 };
 
-let button = document.querySelector('#joke-btn');
+let attachImg = (imgSrc) => {
+	let img = document.createElement('img');
+	img.width = '130';
+	img.height = '130';
+	img.src = imgSrc;
+	document.querySelector('.container').append(img);
+};
+
 button.addEventListener('click', loadJoke);
+document.onload = loadJoke();
